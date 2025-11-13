3dbad59724fb0bb5d1685bfccedf8ac3871b2f67
Juan Carlos Chavez <juanchavezsn@gmail.com>
2025-11-13 13:54:15 -0600

    Modificaciones en clase AdminController y properties

--- src/main/java/com/infotec/springbootjwtroles/demo/AdminController.java
+++ src/main/java/com/infotec/springbootjwtroles/demo/AdminController.java
@@ -1,14 +1,19 @@
 package com.infotec.springbootjwtroles.demo;
 
+import com.infotec.springbootjwtroles.auth.service.AdminUserService;
+import com.infotec.springbootjwtroles.auth.service.UserAccountService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController @RequestMapping("/api/admin")
+@RequiredArgsConstructor
 public class AdminController {
+
+    private final AdminUserService adminUserService;
+
     @GetMapping("/health")
     @PreAuthorize("hasRole('ADMIN')")
     public Map<String,String> health() {
@@ -23,6 +28,18 @@ public class AdminController {
                 "status", "OK",
                 "usuariosActivos", 25,
                 "tareasPendientes", 3
+        );
+    }
+
+    @PostMapping("/assign-role")
+    @PreAuthorize("hasRole('ADMIN')")
+    public Map<String, String> assignRole(@RequestParam String username,@RequestParam String roleName){
+        var updateUser = adminUserService.assignRole(username, roleName);
+
+        return Map.of(
+                "message", "Rol asignado exitosamente",
+                "user", updateUser.getUsername(),
+                "roles", updateUser.getRoles().toString()
         );
     }
 
